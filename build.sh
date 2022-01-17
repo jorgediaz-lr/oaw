66d32c19e9703af26df3980e1fc6ecaf49b1930a
Jorge Díaz <jorge.diaz@liferay.com>
2022-01-17 20:23:29 +0100

    Copy oaw.war file to the docker folder and print the instructions

--- build.sh
+++ build.sh
@@ -3,3 +3,11 @@ set -o nounset
 set -o errexit
 cd oaw
 mvn clean install -P docker -DskipTests
+cd -
+cp portal/target/oaw.war docker/oaw.war
+echo
+echo "INSTRUCTIONS:"
+echo "1. Execute docker-composer up in docker folder"
+echo "2. Go to http://localhost:8080/oaw/diagnostico.html"
+echo "3. The reports are generated inside the docker/fake-smtp-emails folder"
+echo
