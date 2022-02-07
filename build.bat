3f80f63a045c16c05b8188e349ccbb3f64b614f9
Jorge Díaz <jorge.diaz@liferay.com>
2022-02-07 18:37:30 +0100

    Add build.bat script for windows

---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off
+cd oaw
+call mvn clean install -P docker -DskipTests
+cd ..
+copy portal\target\oaw.war docker\oaw.war
+echo:
+echo INSTRUCTIONS:
+echo 1. Execute docker-composer up in docker folder
+echo 2. Go to http://localhost:8080/oaw/diagnostico.html
+echo 3. The reports are generated inside the docker/fake-smtp-emails folder
+echo:
