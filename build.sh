e8e3157d8ed3f36c3283595f7a430e9a0ab4330a
Jorge Díaz <jorge.diaz@liferay.com>
2022-05-05 09:46:34 +0200

    Fix typo

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd -
 cp portal/target/oaw.war docker/oaw.war
 echo
 echo "INSTRUCTIONS:"
-echo "1. Execute docker-composer up in docker folder"
+echo "1. Execute docker-compose up in docker folder"
 echo "2. Go to http://localhost:8080/oaw/diagnostico.html"
 echo "3. The reports are generated inside the docker/fake-smtp-emails folder"
 echo
